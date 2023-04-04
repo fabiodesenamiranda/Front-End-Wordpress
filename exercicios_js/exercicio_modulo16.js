86e124e62550aa6ea3c8b401d836b52086f8bc33
fabiodesenamiranda <84819606+fabiodesenamiranda@users.noreply.github.com>
2023-04-04 19:07:46 -0300

    exercicio do módulo 16

---
+++ exercicios_js/exercicio_modulo16.js
@@ -0,0 +1,140 @@
+// Resolva as operações //
+
+var a = 10;
+var b = 15;
+console.log(a + b);
+
+var a = "10";
+var b = 2;
+console.log(a + b);
+
+var a = "10";
+var b = 2;
+console.log(a * b);
+
+var a = "10";
+var b = 3;
+console.log(a / b);
+
+var a = "10";
+var b = 3;
+console.log(a % b);
+
+var a = "10";
+var b = true;
+console.log(a + b);
+
+var a = 10;
+var b = "10";
+console.log(a == b);
+
+var a = 10;
+var b = "10";
+console.log(a === b);
+
+var a = 10;
+var b = 11;
+console.log(a < b);
+
+var a = 10;
+var b = 12;
+console.log(a > b);
+
+var a = 10;
+var b = 10.1;
+console.log(a <= b);
+
+var a = 10;
+var b = 9.99;
+console.log(a > b);
+
+var a = 10;
+var b = "dez";
+console.log(a != b);
+
+var a = 10;
+var b = true;
+console.log(a + b);
+
+var a = "dez";
+var b = true;
+console.log(a + b);
+
+var a = 10;
+var b = false;
+console.log(a + b);
+
+var a = 10;
+var b = false;
+console.log(a * b);
+
+var a = true;
+var b = true;
+console.log(a + b);
+
+var a = 10;
+console.log(a++);
+
+var a = 10;
+console.log(a--);
+
+var a = 1;
+var b = 1;
+console.log(a & b);
+
+var a = 1;
+var b = 0;
+console.log(a & b);
+
+var a = 0;
+var b = 0;
+console.log(a & b);
+
+var a = 1;
+var b = 0;
+console.log(a & b);
+
+var a = 0;
+var b = 1;
+console.log(a / b);
+
+var a = 5;
+var b = 5;
+var c = 10;
+console.log(a + b == c);
+
+var a = "5";
+var b = "5";
+var c = 10;
+console.log(a + b == c);
+
+var a = "5";
+var b = 2;
+var c = 9;
+console.log(a * b > c);
+
+var a = 10;
+var b = 10;
+var c = 2;
+console.log((a + b) * c);
+
+var a = 10;
+var b = 10;
+var c = 2;
+console.log(a + b * c);
+
+// Responda as perguntas de acordo com as variáveis //
+let cor = "branco";
+let meuCarro = cor == "branco" ? "cinza" : "preto";
+console.log(meuCarro);
+
+cor = "branco";
+meuCarro = cor == "branco" ? "branco" : "preto";
+console.log(meuCarro);
+
+let carro = "preto";
+meuCarro = carro === "preto" ? "branco" : "preto";
+console.log(meuCarro);
+
+let cavalo = carro == "preto" ? "cinza" : "marrom";
+console.log(cavalo);
